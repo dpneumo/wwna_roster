0af7a44581cfb7b270b8381abcbde9f1f1547c3b
Mitch Kuppinger <dpneumo@gmail.com>
2020-10-11 18:44:36 -0500

    Begin building WWNA address seeds: Englishoak Dr

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Address.create([
+  { number: 6000, street: 'Englishoak Dr'},
+  { number: 6002, street: 'Englishoak Dr'},
+  { number: 6003, street: 'Englishoak Dr'},
+  { number: 6004, street: 'Englishoak Dr'},
+  { number: 6005, street: 'Englishoak Dr'},
+  { number: 6006, street: 'Englishoak Dr'},
+  { number: 6007, street: 'Englishoak Dr'},
+  { number: 6008, street: 'Englishoak Dr'},
+  { number: 6009, street: 'Englishoak Dr'},
+  { number: 6010, street: 'Englishoak Dr'},
+  { number: 6011, street: 'Englishoak Dr'},
+  { number: 6012, street: 'Englishoak Dr'},
+  { number: 6014, street: 'Englishoak Dr'},
+  { number: 6015, street: 'Englishoak Dr'},
+  { number: 6016, street: 'Englishoak Dr'},
+  { number: 6018, street: 'Englishoak Dr'},
+
+  { number: 2100, street: 'Englishoak Dr'},
+  { number: 2102, street: 'Englishoak Dr'},
+  { number: 2104, street: 'Englishoak Dr'},
+  { number: 2106, street: 'Englishoak Dr'},
+  { number: 2107, street: 'Englishoak Dr'},
+  { number: 2108, street: 'Englishoak Dr'},
+  { number: 2110, street: 'Englishoak Dr'},
+
+  { number: 2200, street: 'Englishoak Dr'},
+  { number: 2202, street: 'Englishoak Dr'},
+  { number: 2204, street: 'Englishoak Dr'},
+  { number: 2206, street: 'Englishoak Dr'},
+
+  { number: 2300, street: 'Englishoak Dr'},
+  { number: 2304, street: 'Englishoak Dr'},
+  { number: 2307, street: 'Englishoak Dr'},
+  { number: 2308, street: 'Englishoak Dr'},
+  { number: 2315, street: 'Englishoak Dr'},
+  { number: 2316, street: 'Englishoak Dr'},
+])
