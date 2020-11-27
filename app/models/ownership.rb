d5eb1ea8a3f074cc5def6329a452efecf1ab3652
Mitch Kuppinger <dpneumo@gmail.com>
2020-11-27 16:18:03 -0600

    Use Owner in place of Person when dealing with ownerships.

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,7 +1,6 @@
 class Ownership < ApplicationRecord
   belongs_to :property, class_name: 'House', foreign_key: :house_id
   belongs_to :owner, class_name: 'Person', foreign_key: :person_id
-  belongs_to :person
 
   validates :house_id,  presence: true
   validates :person_id, presence: true
