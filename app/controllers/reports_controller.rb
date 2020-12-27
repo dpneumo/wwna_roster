b17ece44d8711f4be81d1fa900d8044ddf7aace5
Mitch Kuppinger <dpneumo@gmail.com>
2020-12-26 23:43:35 -0600

    Drop reports controller and associated views.

--- app/controllers/reports_controller.rb
+++
@@ -1,5 +0,0 @@
-class ReportsController < ApplicationController
-  def homes
-    @pagy, @homes = pagy(House.order(street: :asc, number: :asc).includes(:occupants), items: 10)
-  end
-end
--- test/controllers/reports_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ReportsControllerTest < ActionDispatch::IntegrationTest
-  test "should get homes" do
-    get homes_url
-    assert_response :success
-  end
-
-end
