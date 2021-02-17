fa56a53e29cf7c8c0b3ff7c9560e1053f578596a
Mitch Kuppinger <dpneumo@gmail.com>
2021-02-17 17:45:02 -0600

    Enhance the home page. Improve the table heading for positions index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,10 @@ class WelcomeController < ApplicationController
   end
 
   def home
+  	@current_year = Date.current.year
+  	@last_year = Date.current.years_ago(1).year
+  	@total_dues_current = Contribution.total_for(year: @current_year)
+  	@total_dues_last_year = Contribution.total_for(year: @last_year)
+  	@positions = Position.current_active_posns
   end
 end
