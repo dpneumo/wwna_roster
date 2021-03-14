946986d9cb92cdc92436ea4fbd18200c9594412e
Mitch Kuppinger <dpneumo@gmail.com>
2021-03-14 16:52:48 -0500

    Initial House sort order should be ['street asc', 'number asc']

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,6 +5,7 @@ class HousesController < ApplicationController
   # GET /houses.json
   def index
     @q = House.ransack(params[:q])
+    @q.sorts = ['street asc', 'number asc'] if @q.sorts.empty?
     @pagy, @houses = pagy(@q.result)
   end
 
